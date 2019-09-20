d50970cca51d05833a6dbb28fdff53f516d12058
Santos Gonzalez <sgonzo3@gmail.com>
2019-09-20 09:36:56 -0400

    Updates

---
+++ precache-manifest.02abcd728364de8e81119940aa6d1b7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4012b5a351505b66c5d2da345fbf82",
+    "url": "/freecodecamp-quote-generator/index.html"
+  },
+  {
+    "revision": "b5b8da302b42f398fe97",
+    "url": "/freecodecamp-quote-generator/static/css/main.ff50685d.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/freecodecamp-quote-generator/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "b5b8da302b42f398fe97",
+    "url": "/freecodecamp-quote-generator/static/js/main.4e905b40.chunk.js"
+  },
+  {
+    "revision": "9cfd25224640cab29d7d",
+    "url": "/freecodecamp-quote-generator/static/js/runtime~main.384c89fa.js"
+  }
+]);
